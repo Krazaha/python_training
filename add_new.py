b798e59885e0aaeaef39ca5e731b0d66326e0ff6
zabiyakin-a <krazaha@gmail.com>
2022-12-23 01:11:03 +0300

    Создание вспомогательного класса для нового контакта

---
+++ add_new.py
@@ -0,0 +1,18 @@
+class AddNew:
+
+    def __init__(self, firstname, middlename, lastname, nickname, company, address, homephone, mobilephone,
+                         email, bday, confirmbday, bmonth, confirmbmonth, byear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.email = email
+        self.bday = bday
+        self.confirmbday = confirmbday
+        self.bmonth = bmonth
+        self.confirmbmonth = confirmbmonth
+        self.byear = byear
--- test_add_new.py
+++ test_add_new.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from add_new import AddNew
 
 class TestAddNew(unittest.TestCase):
     def setUp(self):
@@ -15,11 +16,11 @@ class TestAddNew(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_add_new_page(wd)
-        self.add_new_creating(wd, firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Ivani4",
+        self.add_new_creating(wd, AddNew(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Ivani4",
                               company="Roga i kopita", address="Moscow", homephone="912313123123",
                               mobilephone="231231241231", email="asdasd@sdfasdasd", bday="1",
                               confirmbday="//option[@value='1']", bmonth="March",
-                              confirmbmonth="//option[@value='March']", byear="1900")
+                              confirmbmonth="//option[@value='March']", byear="1900"))
         self.logout(wd)
 
     def test_add_empty_new(self):
@@ -27,64 +28,63 @@ class TestAddNew(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_add_new_page(wd)
-        self.add_new_creating(wd, firstname="", middlename="", lastname="", nickname="",
+        self.add_new_creating(wd, AddNew(firstname="", middlename="", lastname="", nickname="",
                               company="", address="", homephone="",
                               mobilephone="", email="", bday="",
                               confirmbday="//option[@value='']", bmonth="-",
-                              confirmbmonth="//option[@value='-']", byear="")
+                              confirmbmonth="//option[@value='-']", byear=""))
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def add_new_creating(self, wd, firstname, middlename, lastname, nickname, company, address, homephone, mobilephone,
-                         email, bday, confirmbday, bmonth, confirmbmonth, byear):
+    def add_new_creating(self, wd, add_new):
         # fill firstname
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(add_new.firstname)
         # fill middlename
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(add_new.middlename)
         # fill lastname
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(add_new.lastname)
         # fill nickname
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(add_new.nickname)
         # fill company
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(add_new.company)
         # fill address
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(add_new.address)
         # fill home phone
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(homephone)
+        wd.find_element_by_name("home").send_keys(add_new.homephone)
         # fill mobile phone
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobilephone)
+        wd.find_element_by_name("mobile").send_keys(add_new.mobilephone)
         # fill email
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email").send_keys(add_new.email)
         # fill birthday
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(bday)
-        wd.find_element_by_xpath(confirmbday).click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(add_new.bday)
+        wd.find_element_by_xpath(add_new.confirmbday).click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(bmonth)
-        wd.find_element_by_xpath(confirmbmonth).click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(add_new.bmonth)
+        wd.find_element_by_xpath(add_new.confirmbmonth).click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(byear)
+        wd.find_element_by_name("byear").send_keys(add_new.byear)
         wd.find_element_by_name("theform").click()
         # press enter to submit adding
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
