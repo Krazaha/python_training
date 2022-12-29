9349a51292b4addcfba6ed8b7eb3413fc1f236c3
zabiyakin-a <krazaha@gmail.com>
2022-12-30 00:28:58 +0300

    Перенос функций из тестов в отдельный класс Application, создание фикстуры и перевод с unittest на pytest

--- test_add_new.py
+++ test_add_new.py
@@ -1,120 +1,28 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from contact import Contact
+from application import Application
 
-class TestAddNew(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-    
-    def test_add_new(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.add_new_creating(wd, Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Ivani4",
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_new(app):
+    app.login(username="admin", password="secret")
+    app.add_new_creating(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Ivani4",
                               company="Roga i kopita", address="Moscow", homephone="912313123123",
                               mobilephone="231231241231", email="asdasd@sdfasdasd", bday="1",
                               confirmbday="//option[@value='1']", bmonth="March",
                               confirmbmonth="//option[@value='March']", byear="1900"))
-        self.logout(wd)
+    app.logout()
 
-    def test_add_empty_new(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.add_new_creating(wd, Contact(firstname="", middlename="", lastname="", nickname="",
+def test_add_empty_new(app):
+    app.login(username="admin", password="secret")
+    app.add_new_creating(Contact(firstname="", middlename="", lastname="", nickname="",
                               company="", address="", homephone="",
                               mobilephone="", email="", bday="",
                               confirmbday="//option[@value='']", bmonth="-",
                               confirmbmonth="//option[@value='-']", byear=""))
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def add_new_creating(self, wd, add_new):
-        self.open_add_new_page(wd)
-        # fill firstname
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(add_new.firstname)
-        # fill middlename
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(add_new.middlename)
-        # fill lastname
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(add_new.lastname)
-        # fill nickname
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(add_new.nickname)
-        # fill company
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(add_new.company)
-        # fill address
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(add_new.address)
-        # fill home phone
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(add_new.homephone)
-        # fill mobile phone
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(add_new.mobilephone)
-        # fill email
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(add_new.email)
-        # fill birthday
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(add_new.bday)
-        wd.find_element_by_xpath(add_new.confirmbday).click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(add_new.bmonth)
-        wd.find_element_by_xpath(add_new.confirmbmonth).click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(add_new.byear)
-        wd.find_element_by_name("theform").click()
-        # press enter to submit adding
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def open_add_new_page(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/label[2]").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+    app.logout()
