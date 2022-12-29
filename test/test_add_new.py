4ff76c4a5b08462bc67e2836fb6aa37c5c4d065b
zabiyakin-a <krazaha@gmail.com>
2022-12-30 01:41:18 +0300

    Выделен класс для управления сессиями

--- test/test_add_new.py
+++ test/test_add_new.py
@@ -10,19 +10,19 @@ def app(request):
     return fixture
 
 def test_add_new(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_creating(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Ivani4",
                               company="Roga i kopita", address="Moscow", homephone="912313123123",
                               mobilephone="231231241231", email="asdasd@sdfasdasd", bday="1",
                               confirmbday="//option[@value='1']", bmonth="March",
                               confirmbmonth="//option[@value='March']", byear="1900"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_new(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_creating(Contact(firstname="", middlename="", lastname="", nickname="",
                               company="", address="", homephone="",
                               mobilephone="", email="", bday="",
                               confirmbday="//option[@value='']", bmonth="-",
                               confirmbmonth="//option[@value='-']", byear=""))
-    app.logout()
+    app.session.logout()
