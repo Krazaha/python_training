7b00eb2b6d696389f11013a037ea1170df453d8b
zabiyakin-a <krazaha@gmail.com>
2023-01-07 19:18:17 +0300

    Переименованы методы создания контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 
-def test_add_new(app):
+def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Ivani4",
                                company="Roga i kopita", address="Moscow", homephone="912313123123",
@@ -10,7 +10,7 @@ def test_add_new(app):
                                confirmbmonth="//option[@value='March']", byear="1900"))
     app.session.logout()
 
-def test_add_empty_new(app):
+def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
                                company="", address="", homephone="",
